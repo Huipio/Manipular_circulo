105cd2c926b0427af4fa46fd5b69a25c4248a960
Huipio <101611469+Huipio@users.noreply.github.com>
2024-02-09 17:31:32 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -13,25 +13,12 @@ document.addEventListener("DOMContentLoaded", function () {
       circle.style.height = newSize;
     });
 
-    circle.addEventListener("click", function () {
-      const randomColor = getRandomColor();
-      circle.style.backgroundColor = randomColor;
-      colorPicker.value = randomColor;
-    });
 
-    circle.addEventListener("dblclick", function () {
+    circle.addEventListener("click", function () {
       circle.style.display = (circle.style.display === "none") ? "block" : "none";
     });
 
-    function getRandomColor() {
-      const letters = "0123456789ABCDEF";
-      let color = "#";
-      for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
-    }
-  });
+});
 
   function desaparecerAparecer() {
     var circle = document.getElementById('circle');
